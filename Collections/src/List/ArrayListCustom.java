0b41c933e18ec5fbe1a5c18fa4eae1183afe08f4
monikaambadasvasal <vasalmonika10@gmail.com>
2026-04-06 21:39:12 +0530

    Custom array list implementation done.

---
+++ Collections/src/List/ArrayListCustom.java
@@ -0,0 +1,48 @@
+package List;
+
+import javax.xml.transform.Source;
+import java.util.Arrays;
+
+public class ArrayListCustom {
+    Object ar[];
+    int count=0;
+
+    public ArrayListCustom(int size) {
+        ar = new Object[size];
+    }
+
+    public void add(Object data){
+         if(count>=ar.length){
+             int newsize= ar.length+(ar.length/2);
+             Object[] newAr=new Object[newsize];
+             ar=Arrays.copyOf(ar, newsize);
+         }
+         ar[count++]=data;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(ar, count));
+    }
+    public Object get(int index){
+        if(count<index){
+            return "Given index is greater than size of array and element is not available at this position";
+        }
+        else{
+            return ar[index];
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayListCustom arrayListCustom = new ArrayListCustom(5);
+        arrayListCustom.add(67);
+        arrayListCustom.add(87);
+        arrayListCustom.add(45);
+        arrayListCustom.add(32);
+        arrayListCustom.add(89);
+        arrayListCustom.add(87);
+        System.out.println(arrayListCustom);
+        System.out.println(arrayListCustom.get(2));
+        System.out.println(arrayListCustom.get(7));
+    }
+}
